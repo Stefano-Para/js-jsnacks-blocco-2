b73995e97fe8e65c3ca62548edb99d2acc8bf5af
Stefano-Para <stefano.zina@gmail.com>
2021-05-11 15:44:07 +0200

    add script

---
+++ js/script-snack-1.js
@@ -0,0 +1,24 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+var biciclette = [
+    {
+        nome: 'super01',
+        peso: 7,
+    },
+    {
+        nome: 'super02',
+        peso: 11,
+    },
+    {
+        nome: 'super03',
+        peso: 15,
+    },
+    {
+        nome: 'super04',
+        peso: 9,
+    },
+];
+console.log(biciclette);
